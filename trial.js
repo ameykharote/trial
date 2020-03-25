c06746dca792ce53090d42f8f50962bf086bb887
ameykharote <ameykharote@gmail.com>
2020-03-25 17:28:41 +0530

    New Problem

--- trial.js
+++ trial.js
@@ -10,3 +10,21 @@ let sum = function (a) {
   return closureFunc;
 }
 console.log(sum(10)(2)(3)(4));
+
+//Creating a array.map() function
+const sample = [1,2,3]
+
+Array.prototype.mymap = function (callback) {
+    const resultArray = [];
+    for (let index = 0; index < this.length; index++) {
+        resultArray.push(callback(this[index], index, this));
+    }
+    return resultArray;
+}
+
+var output = sample.mymap(function(val, index, array) {
+    console.log('val :', val, 'index :', index, 'array :', array);
+    return val * 2;
+})
+
+console.log(output);
