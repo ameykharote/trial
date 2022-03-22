480e338258b688e56dc73b6427ceb34759260378
ameykharote <ameykharote@gmail.com>
2022-03-22 18:05:15 -0400

    Changes

---
+++ jsinterviewqna.js
@@ -0,0 +1,49 @@
+let sum = function (a) {
+  return function (b) {
+    if (b) {
+      return sum(a + b);
+    }
+    return a;
+  };
+};
+
+//es6
+
+let sum1 = (a) => {
+  return (b) => {
+    return b ? sum(a + b) : a;
+  };
+};
+
+//refactor
+
+let sum2 = (a) => (b) => (b ? sum(a + b) : a);
+
+console.log(sum(10)(13)(10));
+
+// let sum = function (a) {
+//   let closureFunc = (b) => (b ? sum(a + b) : a);
+//   closureFunc.toString = function () {
+//     return a;
+//   };
+//   return closureFunc;
+// };
+// console.log(sum(10)(2)(3)(4));
+
+//Creating a array.map() function
+// const sample = [1, 2, 3];
+
+// Array.prototype.mymap = function (callback) {
+//   const resultArray = [];
+//   for (let index = 0; index < this.length; index++) {
+//     resultArray.push(callback(this[index], index, this));
+//   }
+//   return resultArray;
+// };
+
+// var output = sample.mymap(function (val, index, array) {
+//   console.log("val :", val, "index :", index, "array :", array);
+//   return val * 2;
+// });
+
+// console.log(output);
--- starpattern.java
+++ starpattern.java
@@ -1,8 +1,8 @@
 class starpattern
 {
-    int i, j, n = 4;
+    int i, j, n = 5;
     public void printStars(){
-        for(i = n ; i >= 0; i--){
+        for(i = 0 ; i <= n; i++){
             for(j = 0; j <= i; j++)
             {
                 System.out.print("*");
